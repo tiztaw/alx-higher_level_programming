ce07b35d84ef1527e158af8b2f99c5e8358a54c5
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-03-07 20:06:02 +0300

    Update 100-print_comb3.c

--- 0x01-python-if_else_loops_functions/100-print_comb3.c
+++ 0x01-python-if_else_loops_functions/100-print_comb3.c
@@ -1,36 +1,3 @@
-#include <stdio.h>
-/**
- * main - prints out all the numbers between 00 and 99
- * with no two digits being the same
- * Return: 0
- */
-int main(void)
-{
-	int i, j;
-
-	for (i = 48; i < 58; i++)
-	{
-		for (j = i; j < 58; j++)
-		{
-			if (i == j)
-			{
-				continue;
-			}
-
-			putchar(i);
-			putchar(j);
-
-			if (i == 56 && j == 57)
-			{
-				break;
-			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
-}
+#!/usr/bin/python3
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
