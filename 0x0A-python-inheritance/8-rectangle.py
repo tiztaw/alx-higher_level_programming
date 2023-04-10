479299dd1165a79072e4d6a39f47ef87793a4388
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-04-10 16:25:55 +0300

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+"""Rectangle inherits from BaseGeometry
 """
-The class BaseGeometry and subclass Rectangle
-"""
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """A representation of a rectangle"""
+    """Rectangle data inherits from BaseGeometry
+    """
+
     def __init__(self, width, height):
-        """instantiation of the rectangle"""
-        self.integer_validator("width", width)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
-        self.integer_validator("height", height)
-        self.__height = 
+        self.__height = height
