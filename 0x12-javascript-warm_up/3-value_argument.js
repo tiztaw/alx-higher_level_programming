fbb8b5e8954089dde7b1dcc07b7e99f6c0b68ebd
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-06-12 21:28:22 +0300

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-// script that prints the first argument passed to it
-const argv = process.argv[2];
-if (argv === undefined) {
-  console.log('No argument');
-} else {
-  console.log(argv);
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
