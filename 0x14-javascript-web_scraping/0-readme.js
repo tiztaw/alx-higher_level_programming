93d3d82da728d092e87efcba78540b9d0bf14ce0
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-07-25 09:42:32 -0700

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,9 @@
-let header = document.querySelector('header');
-header.style.color = '#FF0000';
+#!/usr/bin/node
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  process.stdout.write(data);
+});
