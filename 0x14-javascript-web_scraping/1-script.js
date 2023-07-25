bbb6b39e41cb83e1b45e0364ed356d50be8f6ee9
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-07-25 09:43:04 -0700

    Update 1-script.js

--- 0x14-javascript-web_scraping/1-script.js
+++ 0x14-javascript-web_scraping/1-script.js
@@ -1,1 +1,11 @@
-$('header').css('color', '#FF0000');
+#!/usr/bin/node
+const fs = require('fs');
+
+const file = process.argv[2] || '';
+const data = process.argv[3] || '';
+
+fs.writeFile(file, data, 'utf8', (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
